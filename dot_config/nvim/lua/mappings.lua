7b297b9fc43be1d240d6796950b3d1f5d3916891
zeroicey <zeroicey.hp@outlook.com>
2026-08-12 15:16:47 +0800

    feat(nvim): add save/quit and file tree keymaps

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -7,4 +7,25 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- save and quit
+map("n", ",w", "<cmd>w<CR>", { desc = "save file" })
+map("n", ",q", "<cmd>q<CR>", { desc = "quit" })
+
+-- file tree: <A-e>
+-- 1. tree not open: open and focus it
+-- 2. inside tree: close it
+-- 3. tree open but not focused: focus it
+map("n", "<A-e>", function()
+	local tree = require("nvim-tree.api").tree
+	if tree.is_tree_buf() then
+		tree.close()
+	elseif tree.is_visible() then
+		tree.focus()
+	else
+		tree.find_file()
+	end
+end, { desc = "toggle/focus file tree" })
+
+-- disable default nvchad file tree keys
+vim.keymap.del("n", "<C-n>")
+vim.keymap.del("n", "<leader>e")
